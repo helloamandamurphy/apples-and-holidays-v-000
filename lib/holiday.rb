bd1582f7cff8bf3e16a023fafc9ef94558fe7885
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-09 16:12:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
@@ -8,7 +6,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter][:christmas] << supply
   holiday_hash[:winter][:new_years] << supply
 end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << supply
@@ -20,17 +17,16 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-holiday_hash[:winter].values.flatten
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  #holiday_array = []
   holiday_hash.each do |season, holiday_details|
     puts "#{season.capitalize}:"
     holiday_details.each do |holiday, supplies|
       puts "  #{holiday.to_s.split("_").collect {|lower_case| lower_case.capitalize!}.join(" ")}: #{supplies.join(", ")}"
-      end
     end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
